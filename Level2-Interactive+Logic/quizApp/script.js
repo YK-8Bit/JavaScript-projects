b4ad49001d87065da990ae29a3eb97b2f81bb57c
YK-8Bit <yk407625@gmail.com>
2025-06-22 23:47:56 +0100

    added a level 2 folder

--- Level2-Interactive+Logic/quizApp/script.js
+++ Level2-Interactive+Logic/quizApp/script.js
@@ -9,7 +9,7 @@ const questionsData = [ //this is the questiions placed in an obect array
         flag: "images/R.png",
         question: "What country does this flag belong to?",
         options: ["Japan", "Pakistan", "Italy", "France"],
-        correct: "Japan"
+        correct: "France"
     },
     {
         flag: "images/R.jpeg",
@@ -44,9 +44,15 @@ const questionsData = [ //this is the questiions placed in an obect array
 ]
 
 let questionIndex = 0; //starts of at index 0 which is the first question
+let scoreCount = 0;//score count is 0
 
 nextBtn.addEventListener("click", () => {
     questionIndex++ //increasiing the index by 1 each time the button is clicked
+    
+    if(questionIndex >= questionsData.length){ //if the question index (0-5) is greater than or equal to 6 
+            alert("Your score is " + scoreCount);
+        }
+
     displayQuestion();
 })
 
@@ -60,6 +66,26 @@ displayQuestion = () => {
         const li = document.createElement("li"); //creates a li element
         li.textContent = option; //for each li set the text to the option
         optionList.appendChild(li);// putting the li inside of the ul to make it appear
+
+        //making the li clickable
+        li.addEventListener("click", (event) => {
+
+        document.querySelectorAll("li").forEach(item => {
+            item.style.pointerEvents = "none"; //disables further clicking
+        })
+        if(li.textContent === currentQuestion.correct){ //checking to see if the question is correct or wrong
+            li.classList.add("correct");//chnages the colour to green if correct
+        }else{
+            li.classList.add("wrong");//changes colour to red if wrong
+        }
+
+        //score logic and  tracking
+        const selectedOption = event.target.textContent; //event = us clicking + target = what element triggered the event + textContent = grab the text of the element
+        if(selectedOption === currentQuestion.correct){ //checking to see if the selected answwer is equal to the correct answer
+            scoreCount++ //increase score by 1
+            score.textContent = "Score: " + scoreCount;
+        }
+        })
     })
 }
 displayQuestion()
