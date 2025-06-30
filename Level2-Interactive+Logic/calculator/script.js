6ccee32754e36be6a8d6474302fb05e54dfc22d6
YK-8Bit <yk407625@gmail.com>
2025-06-30 18:35:14 +0100

    calculator can now calculate expressions

--- Level2-Interactive+Logic/calculator/script.js
+++ Level2-Interactive+Logic/calculator/script.js
@@ -1,6 +1,8 @@
 const answerDisplay = document.getElementById("answerDisplay");
 const questionDisplay = document.getElementById("questionDisplay");
 const buttons = document.querySelectorAll("button");
+const equalButton = document.getElementById("equal");
+
 
 //tracking the expression
 let expression = ""; //expression variable refers to  the math sentnence 
@@ -8,14 +10,26 @@ let expression = ""; //expression variable refers to  the math sentnence
 buttons.forEach(button => { //LOOPING THROUGH EACH BUTTON ELEMENT AND RUNNING A FUNCTION ON IT
     button.addEventListener("click", () => {
         const value = button.textContent; //value variable is equal to the text content of whichever button was clicked 
-
-        if(value === "C" || value === "="){ //checking to see if the value is "C" or "="
-            expression = ""; //make the expression blank
-            answerDisplay.textContent = ""; //make the answer display box empty 
-            questionDisplay.textContent = ""; //make the question display box empty 
-        }else{
-            expression += value;//adding the new number/operator to the curreent expression
+            
+        if(value === "="){
+            //converting the division symbol and multiiplication symbol into mathematical operators
+            const newExpression = expression.replace(/x/g, "*").replace(/÷/g, "/"); // g refers to global meaning everything will change not just the first 1
+            const answer = eval(newExpression);//calculates the expression
+            answerDisplay.value = answer;//displays the asnwer insidee of the answer box
         }
+        else if(value === "C"){ //checking to see if the value is "C" or "="
+             expression = ""; //make the expression blank
+            answerDisplay.value = ""; //make the answer display box empty 
+            questionDisplay.value = ""; //make the question display box empty 
+        }
+        else{
+            expression = expression + value;//adding the new number/operator to the curreent expression 
+        }
+
         questionDisplay.value = expression; //displaying the expression inside of the question display
     })
-});
+
+});  
+
+
+
