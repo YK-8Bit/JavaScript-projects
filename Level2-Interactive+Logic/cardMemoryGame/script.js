4fe142f5ee8202939bc01e83cca38aca453f19dd
YK-8Bit <yk407625@gmail.com>
2025-07-05 20:31:16 +0100

    added shuffling to cards

--- Level2-Interactive+Logic/cardMemoryGame/script.js
+++ Level2-Interactive+Logic/cardMemoryGame/script.js
@@ -1,10 +1,24 @@
-const cards = document.querySelectorAll(".card");
+const cards = Array.from(document.querySelectorAll(".card")); //turns the nodeliist into an actual array
+const container = document.querySelector(".container");
 
 console.log(cards)//testing just to see if it works
 
-for(let i = 0; i < cards.length; i++){
-    cards[i].addEventListener("click", () => {
-        cards[i].classList.toggle("flipped");
+
+//fisher yates shuffle
+shuffle = (array) => {
+    for(let i = array.length - 1; i > 0; i--){ 
+        const random = Math.floor(Math.random() * (i+1));
+        [array[i],array[random]] = [array[random], array[i]]; 
+    }
+}
+
+shuffle(cards) //running shuffle with the array cards
+
+cards.forEach(card => container.appendChild(card))//we need to reappend the cards into the container
+
+for(let i = 0; i < cards.length; i++){ //loops through each element
+    cards[i].addEventListener("click", () => { //liistens for a click on the  card
+        cards[i].classList.toggle("flipped"); //adds a flipped class to the element that gets clicked 
     });
 }
 
